306d6667e0ea65f96ae38a1adadae1e2830046b0
Simone Mongelli <mongelli.simone.2004@gmail.com>
2023-09-18 18:36:52 +0200

    esercizi variabili e operatori

---
+++ main.js
@@ -0,0 +1,43 @@
+// CREAZIONE DI 5 VARIABILI CONTENENTI UN NUMERO 
+let numero1 = 5;
+let numero2 = 3;
+let numero3 = 9;
+let numero4 = 4;
+let numero5 = 7;
+
+//SOMMA
+
+let somma  = (numero1 + numero2 + numero3 + numero4 + numero5) ;
+
+// MEDIA
+let media = (somma / 5);
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media risulta ${media}`);
+
+// ESERCIZIO 2 
+let annonascita = 2004;
+let annocorrente= 2023;
+let età = (annocorrente - annonascita)
+console.log(età);
+
+let eta100 =100;
+let differenza=(eta100-età)
+console.log(differenza);
+//STAMPA 
+console.log(`Hai ${età} anni e ti mancano ${differenza} per compierne ${eta100} `);
+
+//ESERCIZIO 3
+let gatti= 44;
+let fgatti= 6;
+
+let numerofile= (gatti/fgatti)
+console.log(numerofile);
+
+let gattimancanti= (gatti % fgatti)
+console.log(gattimancanti);
+
+let gattirimasti=(fgatti - gattimancanti)
+console.log(gattirimasti);
+
+console.log(`Ci sono ${numerofile} file di gatti e ne mancano ${gattimancanti} per una nuova fila, con un avanzo di ${gattirimasti}`);
+
